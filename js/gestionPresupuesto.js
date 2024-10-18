935f91c78c7f02bca889e9db964a27d3faa44d3d
Víctor Raúl Rodríguez Medina <vicromed.alu.edu.gva.es>
2024-10-18 22:58:40 +0200

    JavaScript II: Función anyadirGasto creada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -62,8 +62,10 @@ function listarGastos() {
     return gastos;
 }
 
-function anyadirGasto() {
-
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
 }
 
 function borrarGasto() {
